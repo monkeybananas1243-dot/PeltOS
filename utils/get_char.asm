c344b82cf0528b43262b739d7a21b760fae3dffd
zowlt <monkeybananas1243@gmail.com>
2025-12-27 20:36:11 -0800

    Add assembly function for keyboard input

---
+++ utils/get_char.asm
@@ -0,0 +1,18 @@
+[BITS 32]
+[GLOBAL get_char]
+
+get_char:
+
+.wait_ready:
+    in al, 0x64
+    and al, 1
+    jz .wait_ready
+
+    in al, 0x60
+    
+    push eax
+    mov ecx, 0xFFFF
+.delay:
+    loop .delay
+    pop eax
+    ret
